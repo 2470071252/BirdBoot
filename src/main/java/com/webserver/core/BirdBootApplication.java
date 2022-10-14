01a872f62ea84b3fc8bda4152ebae352b72aa450
Masterz <2470071252@qq.com>
2022-10-14 09:53:40 +0800

    BirdBoot 2022/10/14

---
+++ src/main/java/com/webserver/core/BirdBootApplication.java
@@ -0,0 +1,12 @@
+package com.webserver.core;
+
+/**
+ * 该项目用来模拟Tomcat+SpringMVC来搭建网络应用
+ * 这里我们会实现Tomcat做一个WebServer与客户端进行HTTP交互
+ * 并模拟SpringMVC的主要工作完成对业务的处理
+ */
+public class BirdBootApplication {
+    public static void main(String[] args) {
+
+    }
+}
