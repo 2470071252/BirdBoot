ff698f579fdb01630b03161c73aa8a329e4746ee
Masterz <2470071252@qq.com>
2022-10-18 16:25:38 +0800

    BirdBoot 2022/10/18 新增学子商城 单例模式

---
+++ V8/src/main/java/com/webserver/core/ClientHandler.java
@@ -0,0 +1,56 @@
+package com.webserver.core;
+
+import com.webserver.http.HttpServletRequest;
+import com.webserver.http.HttpServletResponse;
+
+import javax.print.URIException;
+import java.io.*;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.nio.charset.StandardCharsets.ISO_8859_1;
+
+/**
+ * 当前线程任务负责与指定的客户端进行交互
+ * 这里的交互规则要遵从HTTP协议的交换要求，以客户端进行“一问一答”的交互规则
+ * <p>
+ * 交互过程分三步：
+ * 1.解析请求
+ * 2.处理请求
+ * 3.发送响应
+ */
+public class ClientHandler implements Runnable {
+    private Socket socket;
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // 1.解析请求
+            HttpServletRequest request = new HttpServletRequest(socket);
+            HttpServletResponse response = new HttpServletResponse(socket);
+
+            // 2.处理请求
+           DispatcherServlet.getInstance().service(request,response);
+
+            // 3.发送响应
+            response.response();
+
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ V8/src/main/java/com/webserver/core/DispatcherServlet.java
@@ -0,0 +1,54 @@
+package com.webserver.core;
+
+import com.webserver.http.HttpServletRequest;
+import com.webserver.http.HttpServletResponse;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+/**
+ * 该类实际上是SpringMVC框架提供的类，用于接手Tomcat处理一次Http交互
+ * 处理请求环节的工作，也是Tomcat和SpringMVC框架整合中关键的一个类
+ */
+public class DispatcherServlet {
+    private static DispatcherServlet instance = new DispatcherServlet();
+
+    private static File root;
+    private static File staticDir;
+    static {
+        try {
+            root = new File(ClientHandler.class.getClassLoader()
+                    .getResource(".").toURI() );
+            staticDir = new File(root, "static");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private DispatcherServlet() {
+    }
+
+    public  static DispatcherServlet getInstance(){
+        return instance;
+    }
+
+    public void service(HttpServletRequest request, HttpServletResponse response) throws URISyntaxException {
+        String path = request.getUri();
+        File index = new File(staticDir, path);
+
+            /*
+            如果在static文件夹中找不到对应的文返回404异常页面
+            isFile():
+            true if and only if the file denoted by this abstract pathname exists and is a normal file;
+            false otherwise
+             */
+        if (index.isFile()){
+            response.setContentFile(index);
+        }else {
+            response.setStatusCode(404);
+            response.setStatusReason("NotFound");
+            index = new File(staticDir,"404.html");
+            response.setContentFile(index);
+        }
+    }
+}
